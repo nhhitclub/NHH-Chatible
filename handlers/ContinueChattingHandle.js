2efd1b5527eaf51f1bcfd670768be313cd1a4b66
thienphuc-dev <phucnguyenvuong07@gmail.com>
2022-08-27 21:10:39 +0700

    add handler functions

---
+++ handlers/ContinueChattingHandle.js
@@ -0,0 +1,13 @@
+const { TemplateBuilder } = require("../functions/facebook")
+
+const ContinueChattingHandle = async (messenger, userID, callback = () => {}) => {
+    const templateBuilder = new TemplateBuilder()
+    .setTitle("🤗 Chào mừng bạn đã quay trở lại với NHH Chatible")
+    .setSubtitle("Để bắt đầu, hãy bấm vào nút bên dưới:vv")
+    .addPostbackButton("Bắt đầu", "CHAT_REQUEST")
+
+    await messenger.sendMessageUsingTemplate(userID, templateBuilder)
+    callback();
+}
+
+module.exports = { ContinueChattingHandle }
---
+++ handlers/NewUserRequestHandle.js
@@ -0,0 +1,14 @@
+const { TemplateBuilder } = require("../functions/facebook")
+
+const NewUserRequestHandle = async (messenger, userID, callback = () => {}) => {
+    const templateBuilder = new TemplateBuilder()
+      .setTitle("🤗 Chào mừng bạn lần đầu đã đến với NHH Chatible")
+      .setSubtitle("Trước hết, bạn cần phải chấp nhận điều khoản sử dụng của hệ thống")
+      .addWebviewButton("Điều khoản sử dụng", "https://youtu.be/dQw4w9WgXcQ")
+      .addPostbackButton("Tôi đồng ý", "NEW_USER_START")
+
+    await messenger.sendMessageUsingTemplate(userID, templateBuilder)
+    callback();
+}
+
+module.exports = { NewUserRequestHandle }
