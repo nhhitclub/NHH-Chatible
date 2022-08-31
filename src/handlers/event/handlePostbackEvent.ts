992f22cff92aed24e8071b7c1e546d7c04473e4c
namanhishere <53372786+namanhishere@users.noreply.github.com>
2022-08-31 08:01:00 +0000

    add fix to handlePostbackEvent from switch fallthrough

--- src/handlers/event/handlePostbackEvent.ts
+++ src/handlers/event/handlePostbackEvent.ts
@@ -3,10 +3,17 @@ import EXISTED_USER_START from "../postback/EXISTED_USER_START"
 
 export const handlePostbackEvent = async (mess: any) => {
     
-    switch(mess.postback.payload) {
-        case "CHAT_REQUEST": CHAT_REQUEST(mess)
-        case "EXISTED_USER_START": EXISTED_USER_START(mess)
-    }
+    // switch(mess.postback.payload) {
+    //     case "CHAT_REQUEST":
+    //         CHAT_REQUEST(mess) 
+    //         break
+    //     case "EXISTED_USER_START": 
+    //         EXISTED_USER_START(mess)
+    //         break
+    // }
+
+    if(mess.postback.payload == "CHAT_REQUEST") CHAT_REQUEST(mess)
+    if(mess.postback.payload == "EXISTED_USER_START") EXISTED_USER_START(mess)
 
     
 }
