bcd70dfe3d7343166cc7879fe7ffa00f8eaf41c0
thienphuc-dev <phucnguyenvuong07@gmail.com>
2022-09-01 21:28:25 +0700

    clean the bad words of message

--- src/handlers/message/InChatHandle.ts
+++ src/handlers/message/InChatHandle.ts
@@ -1,20 +1,28 @@
 import { FacebookController, MessageBuilder } from "../../functions/facebook"
 import { Chat } from "../../functions/mongooes";
+import Filter from 'bad-words';
+import { badWords } from "../../lib/vietnamese-badwords";
+
 
 
 export const InChatHandle: Function = async (mess: any, userInDB: any, callback: Function = () => { }) => {
     const fbInstance: FacebookController = FacebookController.getInstance()
-    const chatInDB = await Chat.findOne({ chatID: userInDB?.[0].currentChatID })
+    const chatInDB = await Chat.findOne({ chatID: userInDB.currentChatID })
 
     const userID = mess.sender.id
 
     const chatMembers: Array<string> = chatInDB.members
     const anotherMember: string = chatMembers[(chatMembers.indexOf(userID) + 1) % 2]
     
-
+    
     const messageInfo = mess.message
+
+    const filter = new Filter({ placeHolder: '*' })
+    filter.addWords(...badWords) // chưa loc đc tiếng việt :(
+    const censoredText = filter.clean(messageInfo.text)
+
     if ("text" in messageInfo) {
-        await fbInstance.sendTextOnlyMessage(anotherMember, messageInfo.text)
+        await fbInstance.sendTextOnlyMessage(anotherMember, censoredText)
 
         await chatInDB.chatMess.push({ sender: userID, text: messageInfo.text })
 
@@ -27,7 +35,7 @@ export const InChatHandle: Function = async (mess: any, userInDB: any, callback:
             try{ 
                 await fbInstance.sendMessage(anotherMember, messageBuilder)
             }catch(e) { console.log('error while sending attachment')}
-            //await chatInDB.chatMess.push({ sender: userID, attachmentUrl: attachment.payload.url})
+            await chatInDB.chatMess.push({ sender: userID, attachmentUrl: attachment.payload.url})
 
         });
     }
---
+++ src/lib/vietnamese-badwords.ts
@@ -0,0 +1,22 @@
+//dont open if you dont want to...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const badWords: Array<string> = [ 'cặc','lồn','lồng','địt','địt mẹ','địt con mẹ mày','mé','má','mọe','buồi','đầu buồi','buồi','cu','cứt','con mẹ','vãi','đệch','đéo','đít','dở hơi','khốn','mặt lồn','mặt lờ','đồ khỉ','thằng nhãi','đồ thần kinh','thằng thần kinh']
