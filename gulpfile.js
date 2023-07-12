a89f563ee1e3c1d8f30675f4b5ccd15ad3f92941
namanhishere <ubuntu@Desktop>
2023-07-12 12:37:44 +0700

    Remove gulp by fix tsconfig

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-const ts = require("gulp-typescript");
-var tsProject = ts.createProject('tsconfig.json',{ noImplicitAny: true });
-const {compilerOptions} = require("./tsconfig.json")
-
-gulp.task("default", function () {
-  var tsResult = gulp.src("src/**/*.ts").pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest(compilerOptions.outDir)); 
-});
