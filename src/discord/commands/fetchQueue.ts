e149fb1aae927cbb867ab10315501ad94d2b0678
thienphuc-dev <phucnguyenvuong07@gmail.com>
2023-07-17 21:07:27 +0700

    change let to const

--- src/discord/commands/fetchQueue.ts
+++ src/discord/commands/fetchQueue.ts
@@ -11,16 +11,16 @@ module.exports = {
                 .setName('fetch')
                 .setDescription('Xem danh sách người dùng trong hàng chờ')),
 	async execute(interaction:any) {
-		let queue:Array<string> = QueueManager.getInstance().queue
+		const queue:Array<string> = QueueManager.getInstance().queue
         let outstr:string = ""
-        let count:number = 1
+        const count:number = 1
         for await (const user of queue){
-            let userInfo:any = await User.findOne({user})
+            const userInfo:any = await User.findOne({user})
             
             outstr = outstr+`${count}. **${user}**-`
         }
         console.log(queue)
-        let embed:EmbedBuilder = new EmbedBuilder()
+        const embed:EmbedBuilder = new EmbedBuilder()
         .setColor(0x0099FF)
         .addFields({name:`Hiện trong hàng chờ có: ${queue.length} người dùng`,value:queue.length == 0 ? "Danh sách trống": queue.join("\n")})
 
