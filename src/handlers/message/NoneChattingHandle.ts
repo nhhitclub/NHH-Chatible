d74b6f952f298b3da01a1c4a211e6784269a7a75
thienphuc-dev <phucnguyenvuong07@gmail.com>
2023-07-17 21:11:38 +0700

    remove icon ":v"

--- src/handlers/message/NoneChattingHandle.ts
+++ src/handlers/message/NoneChattingHandle.ts
@@ -3,7 +3,7 @@ import { TemplateBuilder, FacebookController } from "../../functions/facebook"
 export const NoneChattingHandle: Function = async (userID: string, callback: Function = () => {}) => {
     const templateBuilder = new TemplateBuilder()
     .setTitle("🤗 Chào mừng bạn đã đến với với NHH Chatible")
-    .setSubtitle("Để bắt đầu, hãy bấm vào nút bên dưới:vv")
+    .setSubtitle("Để bắt đầu, hãy bấm vào nút bên dưới")
     .addPostbackButton("Bắt đầu", "CHAT_REQUEST")
     await FacebookController.getInstance().sendMessageUsingTemplate(userID, templateBuilder)
     callback();
