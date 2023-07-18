8b948c88f73305e4c15dba3b898b21deb350b5f8
thienphuc-dev <phucnguyenvuong07@gmail.com>
2023-07-18 21:41:14 +0700

    fix bug that handler runs wrong command

--- src/discord/event/slashRegistring.ts
+++ src/discord/event/slashRegistring.ts
@@ -7,16 +7,17 @@ module.exports = {
     once:true,
     execute: async () => {
 
-        let rest:Discord.REST = new Discord.REST({version:"10"}).setToken(process.env.DISCORD_TOKEN)
+        const rest:Discord.REST = new Discord.REST({version:"10"}).setToken(process.env.DISCORD_TOKEN)
+
         try {
-            //console.log(`Started refreshing ${DiscordClient.commands.length} application (/) commands.`);
+            console.log(`Started refreshing ${DiscordClient.commands.length} application (/) commands.`);
             const data:any = await rest.put(
                 Discord.Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
                 { 
-                    body: DiscordClient.commands.map(data=> data.data)
+                    body: DiscordClient.commands.map(data => data.data)
                 },
             );
-            //console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+            console.log(`Successfully reloaded ${data.length} application (/) commands.`);
         } catch (error) {
             console.error(error);
         }
