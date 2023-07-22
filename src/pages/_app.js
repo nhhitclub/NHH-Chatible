bbba48c11e2e88cea0cdc75fb5aeaf42e7643885
thienphuc-dev <phucnguyenvuong07@gmail.com>
2023-07-22 11:34:52 +0700

    add favicon icon, title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Script from 'next/script';
+import Head from 'next/head';
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
   }, []);
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+      <Head>
+        <title>NHH Chatible</title>
+        <link rel="icon" href='//upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Echo_chat_icon.svg/1024px-Echo_chat_icon.svg.png' sizes="any" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 
